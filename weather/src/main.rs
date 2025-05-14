bbd2fcb791809632ba57be31d81f82218683474d
Saarah Nazar <saarahnazar@Saarahs-MacBook-Pro.local>
2025-05-14 16:20:10 -0700

    api key optional

--- weather/src/main.rs
+++ weather/src/main.rs
@@ -1,8 +1,7 @@
 use std::io;
 use serde::Deserialize;
 use colored::*;
-use core::str::FromStr;
-use hifitime::prelude::*;
+use chrono::prelude::*;
 
 //
 // NEW DATA STRUCTURE for JSON response from bytes to readable text
@@ -30,7 +29,7 @@ struct Wind {
 }
 
 //
-// API call, display, main loop
+// API call, display
 //
 fn get_weather_info(city: &str, country_code: &str, api_key: &str) -> Result<WeatherResponse, reqwest::Error> {
     let url = format!(
@@ -70,21 +69,6 @@ fn display_weather_info(response: &WeatherResponse) {
     };
     println!("{}", weather_text_colored);
 
-    let epoch = Epoch::from_gregorian_utc_hms(2015, 2, 7, 11, 22, 33);
-
-    // Parsing with a custom format
-    assert_eq!(
-        Epoch::from_format_str("Sat, 07 Feb 2015 11:22:33", "%a, %d %b %Y %H:%M:%S").unwrap(),
-        epoch
-    );
-
-    // And printing with a custom format
-    let fmt = Format::from_str("%a, %d %b %Y %H:%M:%S").unwrap();
-    assert_eq!(
-        format!("{}", Formatter::new(epoch, fmt)),
-        "Sat, 07 Feb 2015 11:22:33"
-    );
-
 }
 fn get_temperature_emoji(temperature: f64) -> &'static str {
     if temperature < 0.0 {
@@ -100,42 +84,42 @@ fn get_temperature_emoji(temperature: f64) -> &'static str {
     }
 }
 fn main() {
-    println!("{}", "Welcome to Weather Station!".bright_yellow()); // Displaying welcome message
+    println!("{}", "Welcome to your personal ornithologist! You will get advice tailored to your current weather and time of year to spot the most birds on your next expedition.".bright_yellow());
 
-    loop {
-        println!("{}", "Please enter the name of the city:".bright_green()); // Prompting user to enter city name
+    println!("{}", "Please enter the name of the city you are currently in:".bright_green());
 
-        let mut city = String::new();
-        io::stdin().read_line(&mut city).expect("Failed to read input"); // Reading user input for city name
-        let city = city.trim();
+    let mut city = String::new();
+    io::stdin().read_line(&mut city).expect("Failed to read input");
+    let city = city.trim();
 
-        println!("{}", "Please enter the country code (e.g., US for United States):".bright_green()); // Prompting user to enter country code
+    println!("{}", "Please enter the country code you are currently in (e.g., US for United States):".bright_green());
 
-        let mut country_code = String::new();
-        io::stdin().read_line(&mut country_code).expect("Failed to read input"); // Reading user input for country code
-        let country_code = country_code.trim();
+    let mut country_code = String::new();
+    io::stdin().read_line(&mut country_code).expect("Failed to read input");
+    let country_code = country_code.trim();
 
-        // Get your API key from OpenWeatherMap
-        let api_key = ""; 
+    println!("{}", "Please enter your OpenWeatherMap API key. If you do not have one, fear not! You can still get advice tailored to your current season. Just type \"no\" in the field below.".bright_green());
 
+    let mut api_key = String::new();
+    io::stdin().read_line(&mut api_key).expect("Failed to read input");
+    let api_key = api_key.trim();
+
+    let local: DateTime<Local> = Local::now(); // e.g. `2014-11-28 21:45:59.324310806 -09:00`
+
+    println!("Current time: {}", local);
+
+    if api_key != "no" {
         // Calling the function to fetch weather information
         match get_weather_info(&city, &country_code, api_key) {
             Ok(response) => {
-                display_weather_info(&response); // Displaying weather information
+                display_weather_info(&response);
             }
             Err(err) => {
-                eprintln!("Error: {}", err); // Printing error message in case of failure
+                eprintln!("Error: {}", err);
             }
         }
-
-        println!("{}", "Do you want to search for weather in another city? (yes/no):".bright_green()); // Prompting user to continue or exit
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read input"); // Reading user input for continuation
-        let input = input.trim().to_lowercase();
-
-        if input != "yes" {
-            println!("Thank you for using our software!");
-            break; // Exiting the loop if user doesn't want to continue
-        }
     }
+
+    println!("{}", "Thank you for consulting with us! May the force be with you on your upcoming birdwatching expedition!!".bright_blue());
+
 }
